c154854da5860117f1566e375eb5ffe148122558
devopswithrakesh <159764955+devopswithrakesh@users.noreply.github.com>
2025-06-26 22:53:24 +0530

    Add files via upload

--- eks.tf
+++ eks.tf
@@ -1,39 +1,109 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.37.1"
+// Define an IAM role for the EKS cluster control plane
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster"
 
-     cluster_name = local.name 
-     cluster_endpoint_public_access=true
-      
-      cluster_addons = {
-         coredns                = {
-            most_recent=true
-         }
-    
-    kube-proxy             = {
-        most_recent=true
+  // Specify the permissions for assuming this role
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
-    vpc-cni                = {
-        most_recent=true
-    }
-  }
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-    
-    eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      
-      min_size = 1
-      max_size = 2
-      desired_size = 1
-
-      instance_types = [t2.medium"]
-      capacity_type = "SPOT"
-    }
-  }
-
-  tags = local.tags
+  ]
 }
+POLICY
+}
+
+// Attach AmazonEKSClusterPolicy to the IAM role created for EKS cluster
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+// Attach AmazonEKSServicePolicy to the IAM role created for EKS cluster
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+// Create an EKS cluster
+resource "aws_eks_cluster" "aws_eks" {
+  name     = "eks_cluster_demo"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  // Configure VPC for the EKS cluster
+  vpc_config {
+    subnet_ids = ["subnet-053e7234b6f222e5c", "subnet-0889bf10ec9bd6e4e"]
+  }
+
+  // Add tags to the EKS cluster for identification
+  tags = {
+    Name = "EKS_demo"
+  }
+}
+
+// Define an IAM role for EKS worker nodes
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-node-group-demo"
+
+  // Specify the permissions for assuming this role
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+// Attach AmazonEKSWorkerNodePolicy to the IAM role created for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+// Attach AmazonEKS_CNI_Policy to the IAM role created for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+// Attach AmazonEC2ContainerRegistryReadOnly to the IAM role created for EKS worker nodes
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+// Create an EKS node group
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.aws_eks.name
+  node_group_name = "node_demo"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = ["subnet-053e7234b6f222e5c", "subnet-0889bf10ec9bd6e4e"]
+
+  // Configure scaling options for the node group
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  // Ensure that the creation of the node group depends on the IAM role policies being attached
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
